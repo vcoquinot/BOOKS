026db634a847084dbb1287aadc2057b87167b4ff
vcoquinot <vcoquinot@gmail.com>
2017-10-01 12:03:00 +0200

    Add files via upload

---
+++ admin/database.php
@@ -0,0 +1,66 @@
+<?php
+
+class Database
+{
+    private static $dbHost = "localhost";
+    private static $dbName = "books";
+    private static $dbUsername = "root";
+    private static $dbUserpassword = "";
+    
+    private static $connection = null;
+    
+    public static function connect()
+    {
+        if(self::$connection == null)
+        {
+            try
+            {
+              self::$connection = new PDO("mysql:host=" . self::$dbHost . ";dbname=" . self::$dbName , self::$dbUsername, self::$dbUserpassword);
+            }
+            catch(PDOException $e)
+            {
+                die($e->getMessage());
+            }
+        }
+        return self::$connection;
+    }
+    
+    public static function disconnect()
+    {
+        self::$connection = null;
+    }
+
+}
+?>
+
+
+
+
+
+
+
+
+
+
+<?php
+try
+{
+    // On se connecte à MySQL
+    $bdd = new PDO('mysql:host=localhost;dbname=books;charset=utf8', 'root', '');
+}
+catch(Exception $e)
+{
+    // En cas d'erreur, on affiche un message et on arrête tout
+        die('Erreur : '.$e->getMessage());
+}
+
+// Si tout va bien, on peut continuer
+
+// On récupère tout le contenu de la table jeux_video
+$reponse = $bdd->query('SELECT * FROM items');
+
+
+$reponse->closeCursor(); // Termine le traitement de la requête
+
+?>
+
